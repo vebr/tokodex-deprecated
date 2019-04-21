f7ddb81e4d115ff73116e8e5ccbb75a3a5b74bd1
vebr <skye420@keemail.me>
2019-04-21 16:33:45 +0700

    test commit

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -36,7 +36,7 @@ class Home extends React.Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <h1>React.js News</h1>
+          <h1>This is home</h1>
           {loading
             ? 'Loading...'
             : reactjsGetAllNews.map(item => (
